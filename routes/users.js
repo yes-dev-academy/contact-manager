3ccf6b55c32aff33d85a1001a3c33a2c968f229d
yes-dev-academy <63643941+yes-dev-academy@users.noreply.github.com>
2020-04-14 17:20:22 +0430

    add register user to route

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require("express-validator");
+const User = require("../models/users");
 
 // post api/users
-// register 
+// register
 // access
 
-router.post('/',(req,res)=>{
-    res.send('Register a user')
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is requird").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password  with 6 or more charcters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    const{name,email,password} = req.body;
+    try {
+        let user = await User.findOne({email})
+        if (user){
+            return res.status(400).json({msg:"user existed!"})
+        }
+    user = new User({
+        name,
+        email,
+        password
+    })
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password,salt);
+        await user.save();
+        res.send('user saved')
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error')
+    }
+    //res.send("passed successfuly")
+    //res.send('Register a user')
+    //res.send(req.body);
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const app = express();
 //conncet
 connectDB();
 
+//init middleware
+app.use(express.json({extended:false}))
+
 app.get('/',(req,res)=>res.json({msg:'Welcome to contact manger Home page'}));
 
-
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
 //Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
