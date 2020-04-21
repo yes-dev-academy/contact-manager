4111382444d4cb74295d922d1471ba764a5eead6
yes-dev-academy <63643941+yes-dev-academy@users.noreply.github.com>
2020-04-21 17:28:25 +0430

    Finished Backend & start Front react
    next step

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {Link } from "react-router-dom";
+
+const Navbar = ({ title }) => {
+  return (
+    <div className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a className="navbar-brand" href="/">{title}</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+      <li className="nav-item active">
+        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+      </li>
+      <li className="nav-item active">
+        <Link className="nav-link" to="/about">About</Link>
+      </li>
+    </ul>
+   
+  </div>
+</div>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "Contact Manager",
+};
+export default Navbar;
--- routes/contacts.js
+++ routes/contacts.js
@@ -5,7 +5,7 @@ const { check, validationResult } = require("express-validator");
 const Contacts = require('../models/contacts');
 const Users = require('../models/users');
 // get api/contacts
-// get all contacts 
+// get all contacts cd
 // access public
 
 router.get('/',auth,async(req,res)=>{
@@ -22,18 +22,73 @@ router.get('/',auth,async(req,res)=>{
 
 //add new contact
 
-router.post('/',(req,res)=>{
-    res.send('add contact')
+router.post('/',[auth,[
+    check('name','name is requierd').not().isEmpty()
+]],async(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    const {name,email,phone,type} = req.body;
+    try {
+      const newContact = new Contacts({
+          name,email,phone,type, user:req.user.id
+      }) 
+      
+      const contact = await newContact.save()
+      res.json(contact)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send({msg:'server error'});  
+    }
+    //res.send('add contact')
 });
 
 //update contact/:id
-router.put('/:id',(req,res)=>{
-    res.send('update contact')
+router.put('/:id',auth,async(req,res)=>{
+    const {name,email,phone,type} = req.body;
+    const contactField ={};
+    if(name) contactField.name = name;
+    if(email) contactField.email = email;
+    if(phone) contactField.phone = phone;
+    if(type) contactField.type = type;
+
+    try {
+        let contact = await Contacts.findById(req.params.id)
+        if(!contact) returnre.status(404).json({msg:"Contact Not Found"})
+        //check user owns contact
+        if (contact.user.toString()!== req.user.id){
+            return res.status(401).json({msg:"Not Authorized!!"})
+        }
+
+        contact = await Contacts.findByIdAndUpdate(req.params.id,
+            {$set: contactField},{new:true})
+            res.json(contact);//updated contact
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send({msg:'server error'});
+    }
+    //res.send('update contact')
 });
 
 //delete contact/:id
-router.delete('/:id',(req,res)=>{
-    res.send('delete contact')
+router.delete('/:id',auth,async(req,res)=>{
+
+    try {
+        let contact = await Contacts.findById(req.params.id)
+        if(!contact) returnre.status(404).json({msg:"Contact Not Found"})
+        //check user owns contact
+        if (contact.user.toString()!== req.user.id){
+            return res.status(401).json({msg:"Not Authorized!!"})
+        }
+
+        await Contacts.findByIdAndRemove(req.params.id)
+            res.json({msg:"contact removed"});//updated contact
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send({msg:'server error'});
+    }
+    //res.send('delete contact')
 });
 
 module.exports = router;
